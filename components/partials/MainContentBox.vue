0e413a62a799938afd2ff39b5a97637bd4a0abf9
Joseph Richey <josephrichey@me.com>
2019-04-18 12:47:39 -0400

    Added loading and more support for post types (text posts now supported)

--- components/partials/MainContentBox.vue
+++ components/partials/MainContentBox.vue
@@ -1,34 +1,82 @@
 <template>
     <div class="main-content-box">
-        <div
-            v-if="content">
-                <h1>{{ content[0].data.children[0].data.title }}</h1>
+        <!-- LOADING BOILERPLATE -->
+        <div v-bind:class="loading ? 'loading' : 'loaded'">
 
+        </div>
+        
+        <!-- POST LOADED -->
+        <div v-if="shouldShow">
+
+            <!-- TITLE -->
+            <h1>{{ postData.title }}</h1>
+
+            <!-- SELF POST -->
+            <div v-if="postData.is_self">
+                <div class="text" v-html="fixHTML(postData.selftext_html)"></div>
+            </div>
+
+            <!-- LINK/MEDIA POST -->
+            <div v-else>
                 <!-- MEDIA/OEMBED DETECTED -->
-                <div v-if="content[0].data.children[0].data.media" class="media">
-                    <div class="html" v-html="fixEmbed(content[0].data.children[0].data.media.oembed.html)"></div>
+                <div v-if="postData.media" class="media">
+                    <div class="html" v-html="fixHTML(postData.media.oembed.html)"></div>
+                </div>
+
+                <!-- IMAGE DETECTED -->
+                <div v-else-if="postData.url" class="image">
+                    <img v-bind:src="postData.url">
                 </div>
 
                 <div v-else>
                     Post Type Currently Not Supported.
                 </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import {mapState} from 'vuex';
+
 export default {
+    data: function () {
+        return {
+            postData: {}
+        }
+    },
     props: [
         'content'
     ],
+    computed: {
+        shouldShow(){
+            console.log(this.postData);
+            console.log(this.loading);
+            return this.postData && !this.loading;
+        },
+        ...mapState({
+            loading : "loading"
+        })
+    },
     methods: {
-        fixEmbed(html){
+        fixHTML(html){
             var fixedHTML = html;
             fixedHTML = fixedHTML.replace(/amp;/g, '');
             fixedHTML = fixedHTML.replace(/&lt;/g, '<');
             fixedHTML = fixedHTML.replace(/&gt;/g, '>');
-            console.log(fixedHTML);
             return fixedHTML;
+        },
+        updatePostData(dataProp){
+            this.postData = dataProp;
+        }
+    },
+    watch: {
+        content: function(){
+            // Map that long object of BS to postData
+            console.log("UPDATE DETECTED");
+            if(this.content){
+                this.updatePostData(this.content[0].data.children[0].data);
+            }
         }
     }
 }
@@ -36,8 +84,13 @@ export default {
 
 <style lang="scss">
     .main-content-box{
+        border-bottom: 1px dotted #DDD;
+
         h1{
             margin: 0 0 20px;
+            background: rgb(237, 237, 237);
+            padding: 10px 20px;
+            border-radius: 5px;
         }
 
         .media{
@@ -50,6 +103,23 @@ export default {
                 left: 0;
                 width: 100%;
                 height: 100%;
+            }
+        }
+
+        .image{
+            img{
+                width: 100%;
+            }
+        }
+
+        .text{
+            padding: 0 10px;
+            line-height: 1.5;
+            font-weight: 300;
+            font-size: 0.8em;
+
+            p{
+                margin: 0 0 20px;
             }
         }
     }
